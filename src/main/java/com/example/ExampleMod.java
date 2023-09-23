1b77869d75ad817ae47d1a30d57c7078bb5be3ae
ErrorPower <ErrorPower2001@outlook.com>
2023-09-23 16:31:25 +0800

    - 修改 mod 版本号为 0.0.1 - 恢复 com.example.EaxmpleMain.java，仅为其增加注释；   将 fabric wiki 的添加物品教程的代码移动到 pers.errorpower.Initializer - 将 pers.errorpower.Initializer 加入 fabric.mod.json

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -7,27 +7,14 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Fabric 物品设置
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-// Minecraft 物品功能
-import net.minecraft.item.Item;
-// Minecraft 注册表
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-// Minecraft 命名空间
-import net.minecraft.util.Identifier;
-
-// Mod 入口
+// Mod 入口 Initializer，
+// 必须要实现 ModInitializer、ClientInitializer 和 ServerInitializer 这三个接口中的一个
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
-    // 新物品的实例
-    public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
-
-	// Minecraft 加载期执行
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -35,8 +22,5 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
-		
-		// 注册物品
-        Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
 	}
 }
